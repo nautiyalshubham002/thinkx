89337444da06e30b6b256f6b6ce20f0c2806d922
nautiyalshubham002 <45534685+nautiyalshubham002@users.noreply.github.com>
2024-09-26 21:29:24 +0530

    Update Play.js

--- src/Play.js
+++ src/Play.js
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Box, Button, Modal, Snackbar, Stack, Typography } from "@mui/material";
 import Header from "./Header";
 import Tile from "./Tile";
 import { useEffect, useState } from "react";
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import Keyboard from "./Kayboard";
 const Play = ({ word }) => {
   const [active, setActive] = useState(0);
+  const [win, setWin] = useState(false);
   const [lettersUsed, setLetterUsed] = useState([]);
   const navigate = useNavigate();
   useEffect(() => {
@@ -33,13 +34,67 @@ const Play = ({ word }) => {
               active={index === active}
               word={word}
               onSubmit={(keys) => {
+                if (keys?.join("") === word?.join("")) {
+                  setActive(6);
+                  setWin(true);
+                }
                 setLetterUsed([...new Set([...lettersUsed, ...keys])]);
                 setActive(active + 1);
               }}
             />
           );
         })}
-
+        <Modal
+          open={win || true}
+          onClose={() => {}}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: 400,
+              bgcolor: "background.paper",
+              border: "1px solid #000",
+              boxShadow: 24,
+              p: 4,
+            }}
+          >
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
+              sx={{
+                color: "orange",
+              }}
+            >
+              SPLENDID
+            </Typography>
+            <Stack
+              sx={{
+                width: "100%",
+                justifyContent: "end !important",
+                flexDirection: "row",
+              }}
+            >
+              <Button
+                sx={{
+                  color: "orange",
+                  border: "1px solid orange",
+                }}
+                variant="outlined"
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Next
+              </Button>
+            </Stack>
+          </Box>
+        </Modal>
         <Keyboard lettersUsed={lettersUsed} word={word} />
       </Stack>
     </Stack>
